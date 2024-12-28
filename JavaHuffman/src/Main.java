f354fff6823e584e565b50e317dda18cdd6334f3
Sergey <sergejtiskovec311@gmail.com>
2024-12-28 03:55:21 +0300

    implement the program launch from the cd

--- JavaHuffman/src/Main.java
+++ JavaHuffman/src/Main.java
@@ -3,24 +3,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите команду encode/decode");
-        String command = scanner.next();
-        System.out.println("Введите имя входного файла");
-        String inputFile = scanner.next();
-        System.out.println("Введите имя выходного файла");
-        String outputFile = scanner.next();
+        if (args.length != 3) {
+            System.out.println("Использование:");
+            System.out.println("Для кодирования: java HuffmanCoding encode inputFile outputFile");
+            System.out.println("Для декодирования: java HuffmanCoding decode inputFile outputFile");
+            return;
+        }
+        String command = args[0];
+        String inputFile = args[1];
+        String outputFile = args[2];
 
         Huffman huffman = new Huffman();
-        if (command.equals("encode")) {
-            huffman.encode(inputFile, outputFile);
-        } else if (command.equals("decode")) {
-            huffman.decode(inputFile, outputFile);
-        } else {
-            System.out.println("Неверная команда. Используйте 'encode' или 'decode'.");
+        try{
+            if (command.equalsIgnoreCase("encode")) {
+                huffman.encode(inputFile, outputFile);
+            } else if (command.equalsIgnoreCase("decode")) {
+                huffman.decode(inputFile, outputFile);
+            } else {
+                System.out.println("Неверная команда. Используйте 'encode' или 'decode'.");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Произошла ошибка: " + e.getMessage());
         }
-
-        scanner.close();
     }
 }
